3143e02faabebdd65000b95d2057ca8acf640029
Joshua Borillo <borilloj@student.ubc.ca>
2026-02-11 15:51:08 -0800

    Added class-level comment, fields, and methods to CategorySummary

--- src/main/model/CategorySummary.java
+++ src/main/model/CategorySummary.java
@@ -1,5 +1,37 @@
 package model;
 
+// Represents a summary of spending for a single category, including totalSpent and numExpenses
 public class CategorySummary {
+
+    private Category category;
+    private double totalSpent;
+    private int numExpenses;
+
+    // EFFECTS: constrcuts a summary for a given category with zero spending and zero number of expenses
+    public CategorySummary(Category category) {
+        // stub
+    }
+
+    // REQUIRES: amount > 0
+    // MODIFIES: this
+    // EFFECTS: adds a given amount to the totalSpent and increments numExpenses
+    public void addExpense(double amount) {
+        // stub
+    }
+
+    // EFFECTS: returns the category this summary is for
+    public Category getCategory() {
+        return null; // stub
+    }
+
+    // EFFECTS: returns the total amount spent in this category
+    public double getTotalSpent() {
+        return 0; // stub
+    }
+
+    // EFFECTS: returns the number of expenses in this category
+    public int getNumExpenses() {
+        return 0; // stub
+    }
 
 }
--- src/main/model/ExpenseManager.java
+++ src/main/model/ExpenseManager.java
@@ -14,12 +14,14 @@ public class ExpenseManager {
         // stub
     }
 
+    // REQUIRES: expense > 0
     // MODIFIES: this
     // EFFECTS: adds the given expense to the list of expenses
     public void addExpense(Expense expense) {
         // stub
     }
 
+    // REQUIRES: 1 <= month && month <= 12
     // EFFECTS: returns a MonthlySummary for the given month and year
     public MonthlySummary getMonthlySummary(int month, int year) {
         return null; // stub
