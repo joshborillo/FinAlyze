7ae0844090c364dc6b722c2d64b5610edf551f0a
Joshua Borillo <borilloj@student.ubc.ca>
2026-02-11 14:37:39 -0800

    Added fields, constructor, and methods (spefications)

--- src/main/model/Category.java
+++ src/main/model/Category.java
@@ -1,6 +1,16 @@
 package model;
 
-// Represents a spending category
+// Represents a spending category that an expense can belong to (ie. Food, Travel, Entertainment)
 public class Category {
+    private String name;
 
+    // EFFECTS: constructs a Category with given name
+    public Category(String name) {
+        // stub
+    }
+
+    // EFFECTS: returns the name of this category
+    public String getName() {
+        return null; // stub
+    }
 }
