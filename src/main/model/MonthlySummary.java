e4d3e6869d17168422c112b634198a010ed50282
Joshua Borillo <borilloj@student.ubc.ca>
2026-02-15 17:22:35 -0500

    Implemented the MonthlySummary class

--- src/main/model/MonthlySummary.java
+++ src/main/model/MonthlySummary.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Map;
+import java.util.HashMap;
 
 // Represents a summary of all spending for a given month and year, totalSpent, and categoryTotals
 public class MonthlySummary {
@@ -12,32 +13,42 @@ public class MonthlySummary {
 
     // EFFECTS: constructs a summary for the given month and year wiht zero spending and empty category totals
     public MonthlySummary(int month, int year) {
-        // stub
+        this.month = month;
+        this.year = year;
+        this.totalSpent = 0;
+        this.categoryTotals = new HashMap<>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds the given amount to the total spent and updates the total for given category
     public void addExpense(Category category, double amount) {
-        // stub
+        totalSpent += amount;
+
+        if (categoryTotals.containsKey(category)) {
+            double currentCategory = categoryTotals.get(category);
+            categoryTotals.put(category, currentCategory + amount);
+        } else {
+            categoryTotals.put(category, amount);
+        }
     }
 
     // EFFECTS: returns the total amount spent in this month
     public double getTotalSpent() {
-        return 0; // stub
+        return totalSpent;
     }
 
     // EFFECTS: returns the map of category totals for this month
     public Map<Category, Double> getCategoryTotals() {
-        return null;
+        return categoryTotals;
     }
 
     // EFFECTS: returns the month this summary represents
     public int getMonth() {
-        return 0; // stub
+        return month;
     }
 
     // EFFECTS: returns the year this summary represents
     public int getYear() {
-        return 0; // sutb
+        return year;
     }
 }
