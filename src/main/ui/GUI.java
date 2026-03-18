cf9ae5a1bc1856fa54a7188555e94249ad9b970f
Joshua Borillo <borilloj@student.ubc.ca>
2026-03-18 13:32:54 -0700

    Add specification

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -7,6 +7,11 @@ public class GUI extends JFrame {
     private ExpenseListPanel tablePanel;
     private JButton addButton;
 
+    /*
+    Main graphical user interface for the app. This class represents the main window (JFrame), 
+    manages user interactions through buttons and menus, and handles persistent data storage (saving/loading). 
+    It organizes the layout by nesting sub-panels like the ExpenseListPanel, and handles top-level action events.
+     */
     public GUI() {
         super("FinAlyze");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
