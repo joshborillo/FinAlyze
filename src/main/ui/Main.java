10676b9a22b8b39a92e7d66834f432152dc1dc90
Joshua Borillo <borilloj@student.ubc.ca>
2026-03-18 14:09:47 -0700

    Added MenuBar and customized it to fit Apple's modern look

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
